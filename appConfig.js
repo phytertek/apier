29ecf02e0d585abb0aa9eb91ac676bdc9bb3ed91
phytertek <ryan.phytertek@gmail.com>
2017-11-24 16:24:54 -0500

    rebase w/o todo or docter/heroku - in prep gor create-apier-cli git update

--- appConfig.js
+++ appConfig.js
@@ -1,52 +1,6 @@
 module.exports = {
   // App Components
   Components: {
-    Todo: {
-      Schema: {
-        Todo: `{
-          title: {
-            type: String,
-            required: true
-          },
-          body: {
-            type: String,
-            required: true
-          },
-          owner: {
-            type: Types.ObjectId,
-            ref: 'User',
-            required: true
-          },
-          complete: {
-            type: Boolean,
-            default: false
-          },
-          stage: {
-            type: String,
-            enum: {
-              values: [
-                'Not Started',
-                'Started',
-                'Blocked',
-                'Under Review',
-                'Testing',
-                'Complete'
-              ]
-            },
-            default: 'Not Started'
-          }
-        }`
-      },
-      Routes: {
-        '/todo': [
-          'POST /todo/t/new - *auth - create new todo',
-          'POST /todo/t/:id - *auth *admin - update todo by id',
-          'GET /todo/t/all - *auth - get all todos owned by authorized user',
-          'GET /todo/t/:id - *auth - get todo by id',
-          'DELETE /todo/t:id - *auth *admin - delete todo by id'
-        ]
-      }
-    },
     Auth: {
       Schema: 'authentication user schema',
       Services: 'authenticatedRoute and decodeToken',
