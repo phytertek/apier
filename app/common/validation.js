8de4190f731d8c5fd37b715ff536080a7cc27a92
phytertek <ryan.phytertek@gmail.com>
2017-12-05 19:08:57 -0500

    added validator package

--- app/common/validation.js
+++ app/common/validation.js
@@ -1,9 +1,14 @@
+const isEmail = require('validator/lib/isEmail');
+
 module.exports = {
   requireFields: fields => {
     Object.keys(fields).forEach(key => {
       if (!fields[key]) {
-        throw new Error(`${Object.keys(fields).join(', ')} are required`)
+        throw new Error(`${Object.keys(fields).join(', ')} are required`);
       }
-    })
+    });
+  },
+  validateEmail: field => {
+    if (!!!isEmail(field)) throw new Error('Not a valid email address');
   }
-}
+};
